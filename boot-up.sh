9aef59d1b59a649bb3801cc9384555a5d210179a
Eduardo Rodriguez <eduardo@Eduardos-MBP.fritz.box>
2023-08-17 17:46:11 +0200

    Add difftastic and lf to required packages
    Install both packages with brew

--- boot-up.sh
+++ boot-up.sh
@@ -2,7 +2,7 @@
 # Eduardo Rodriguez [@erodrigufer] 2023 (c) 
 
 # Basic apps
-brew install tmux go gh neovim
+brew install tmux go gh neovim lf difftastic
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
