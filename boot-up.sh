82054ab92004641da05c48fc90d36e075df8e40f
erodrigufer <erodrigufer@gmail.com>
2023-08-25 15:23:28 +0200

    Add direnv to boot-up packages

--- boot-up.sh
+++ boot-up.sh
@@ -2,7 +2,7 @@
 # Eduardo Rodriguez [@erodrigufer] 2023 (c) 
 
 # Basic apps
-brew install tmux go gh neovim lf difftastic node yarn
+brew install tmux go gh neovim lf difftastic node yarn direnv
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
