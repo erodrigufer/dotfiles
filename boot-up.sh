6c217046f10c77e5884591490ad64d015ed07a11
erodrigufer <erodrigufer@gmail.com>
2023-08-30 16:26:12 +0200

    Add global typescript installation and refactor code

--- boot-up.sh
+++ boot-up.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # Eduardo Rodriguez [@erodrigufer] 2023 (c) 
 
-# Basic apps
-brew install tmux go gh neovim lf difftastic node yarn direnv
+PACKAGES="tmux go gh neovim lf difftastic node yarn direnv"
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Basic apps.
+brew install $(PACKAGES) && "The following packages were properly installed: $(PACKAGES)"
+
+# Install typescript globally.
+npm install --global typescript && echo "typescript was properly installed!"
+
+# Install oh-my-zsh.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && echo "oh-my-zsh was properly installed!"
