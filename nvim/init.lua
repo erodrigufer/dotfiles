0ac46ffbce08c9d4ce1f11c7070fc3f68045b1d8
erodrigufer <erodrigufer@gmail.com>
2023-09-02 00:05:45 +0200

    Use relative line numbering in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,4 @@
+print('init.lua loaded!')
+-- File with general config options.
+require('base')
 require('plugins')
---
+++ nvim/lua/base.lua
@@ -0,0 +1,13 @@
+-- @erodrigufer
+-- General config options --
+
+print('base loaded!')
+-- Turn the bell off for all events.
+vim.opt.belloff = 'all'
+
+-- Relative numbering of lines related to the cursor
+-- position.
+-- The two options must be present, so that the current
+-- cursor line shows the absolute line number.
+vim.opt.number = true
+vim.opt.relativenumber = true
