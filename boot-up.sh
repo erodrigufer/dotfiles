dda7c0e5866b2d81e74f1fb7681c5b3773ca6936
erodrigufer <erodrigufer@gmail.com>
2023-09-01 18:07:59 +0200

    Install extra features of fzf

--- boot-up.sh
+++ boot-up.sh
@@ -6,6 +6,9 @@ PACKAGES="tmux go gh neovim lf difftastic node yarn direnv fzf act glow"
 # Basic apps.
 brew install $(PACKAGES) && "The following packages were properly installed: $(PACKAGES)"
 
+# To install useful key bindings and fuzzy completion for fzf:
+$(brew --prefix)/opt/fzf/install
+
 # Install typescript globally.
 npm install --global typescript && echo "typescript was properly installed!"
 
