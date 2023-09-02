8873488bc7e539492bae0a61729911817aba5da8
erodrigufer <erodrigufer@gmail.com>
2023-09-02 02:27:36 +0200

    Working LSP for Go and TS

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,10 +17,6 @@ packer.startup(function(use)
   'nvim-telescope/telescope.nvim', tag = '0.1.2',
   requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use 'nvimdev/lspsaga.nvim' -- LSP UIs
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
   -- Plugins go here!!
 end)
--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -1,11 +1,7 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup {}
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
-
+lspconfig.gopls.setup{}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -39,7 +35,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
+    vim.keymap.set('n', '<leader>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
   end,
