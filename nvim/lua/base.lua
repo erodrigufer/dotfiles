2d47b1b0a1a2e93f90c497c6d5dc4d9e2f918212
erodrigufer <erodrigufer@gmail.com>
2023-09-03 20:19:59 +0200

    Fix tabs and spaces in neovim

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -11,3 +11,12 @@ vim.opt.belloff = 'all'
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Tabs and spaces.
+-- A tab will be exchanged for spaces.
+vim.opt.expandtab = true
+-- How many columns of whitespace is a \t char worth?
+vim.opt.tabstop = 2
+-- How many columns of whitespace a “level of indentation” is worth?
+vim.opt.shiftwidth = 2
+-- How many columns of whitespace is a tab keypress or a backspace keypress worth?
+vim.opt.softtabstop = 2
