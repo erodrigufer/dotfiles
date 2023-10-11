228130e7b1fbe63606ad4b717f209accca17f6b7
erodrigufer <erodrigufer@gmail.com>
2023-10-11 17:31:58 +0200

    Configure bg color of bufferline

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -3,18 +3,19 @@ if (not status) then return end
 
 local frappe = require("catppuccin.palettes").get_palette "frappe"
 bufferline.setup {
-    highlights = require("catppuccin.groups.integrations.bufferline").get {
-        styles = { "italic", "bold" },
-        custom = {
-            all = {
-                fill = { bg = "#000000" },
-            },
-            frappe = {
-                background = { fg = frappe.text },
-            },
-            latte = {
-                background = { fg = "#000000" },
-            },
-        },
+  highlights = require("catppuccin.groups.integrations.bufferline").get {
+    styles = { "italic", "bold" },
+    custom = {
+      all = {
+        fill = { bg = "#303446" },
+      },
+      frappe = {
+        background = { fg = frappe.text },
+        -- background = { bg = "#303446" },
+      },
+      latte = {
+        background = { fg = "#303446" },
+      },
     },
+  },
 }
