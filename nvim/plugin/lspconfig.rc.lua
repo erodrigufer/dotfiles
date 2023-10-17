0dbd9c349f9e6c003f24c2dbaf707d68c357c965
erodrigufer <erodrigufer@gmail.com>
2023-10-17 12:18:13 +0200

    Add Bash LSP, change symbols for diagnostics

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -139,6 +139,12 @@ nvim_lsp.terraformls.setup {
   capabilities = capabilities
 }
 
+-- Bash LSP.
+nvim_lsp.bashls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
@@ -148,8 +154,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
--- Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+-- Diagnostic symbols in the sign column (gutter).
+local signs = { Error = "✖ ", Warn = " ", Hint = "⌘ ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
