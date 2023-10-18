972883d91cc8d7f318645d47eeb2f3fd6f363bbc
erodrigufer <erodrigufer@gmail.com>
2023-10-18 18:04:27 +0200

    eslint_d is not a valid option for required packages

--- nvim/plugin/0_mason.rc.lua
+++ nvim/plugin/0_mason.rc.lua
@@ -17,6 +17,6 @@ mason.setup({
 masonLspconfig.setup {
   automatic_installation = true,
   ensure_installed = { "awk_ls", "bashls", "arduino_language_server", "astro", "cssls", "dockerls",
-    "docker_compose_language_service", "eslint_d", "eslint", "gopls", "jsonls", "tsserver", "lua_ls", "html",
+    "docker_compose_language_service", "eslint", "gopls", "jsonls", "tsserver", "lua_ls", "html",
     "terraformls" }
 }
