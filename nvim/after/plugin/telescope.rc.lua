8c58e19913fa5ea52d7431a32b4f423121bb2b05
erodrigufer <erodrigufer@gmail.com>
2023-12-08 15:00:37 +0100

    Use which-keys to map Telescope commands

--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -1,21 +1,5 @@
 local status, telescope = pcall(require, "telescope")
 if (not status) then return end
 
-local builtin = require('telescope.builtin')
-local keymap = vim.keymap
--- Fuzzy search through the output of git ls-files
--- command, respects .gitignore
-keymap.set('n', '<Space>g', builtin.git_files, {})
-
--- Lists files in your current working directory,
--- respects .gitignore
-keymap.set('n', '<Space>f', builtin.find_files, {})
-
-keymap.set('n', '<Space>l', builtin.live_grep, {})
-
--- keymap.set('n', '<Leader>fb', builtin.buffers, {})
-
 -- Load the file_browser extension for telescope.
 telescope.load_extension "file_browser"
--- Open file_browser with the path of the current buffer.
-keymap.set('n', '<Space>t', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { noremap = true })
--- nvim/after/plugin/which-key.rc.lua
+++ nvim/after/plugin/which-key.rc.lua
@@ -2,3 +2,14 @@ local status, wk = pcall(require, "which-key")
 if (not status) then return end
 
 wk.setup {}
+
+wk.register({
+  t = {
+    name = "Telescope commands",
+    f = { "<cmd> Telescope find_files <cr>", "Find files in current working directory" },
+    g = { "<cmd> Telescope git_files <cr>", "Find tracked files in current git repo" },
+    l = { "<cmd> Telescope live_grep <cr>", "Grep a string in the files of a repo" },
+    r = { "<cmd> Telescope registers <cr>", "Find string stored in registers" },
+    b = { "<cmd> Telescope  file_browser path=%:p:h select_buffer=true<cr>", "Open the Telescope file browser at path of current buffer", noremap = true }
+  },
+}, { prefix = "<Space>" })
