56bb00c145c3064dac9b8ec643df5aeaa0372533
erodrigufer <erodrigufer@gmail.com>
2024-08-30 11:35:04 +0200

    nvim: Update typescript keymappings to current which-key format

--- nvim/ftplugin/typescriptreact.lua
+++ nvim/ftplugin/typescriptreact.lua
@@ -1,11 +1,9 @@
 local status, wk = pcall(require, "which-key")
 if (not status) then return end
 
-wk.register({
-  t = {
-    name = "Typescript commands",
-    t = { "<cmd>TSC<cr>", "Transpile TS" },
-    m = { "<cmd>TypescriptAddMissingImports<cr>", "Add missing imports" },
-    r = { "<cmd>TypescriptRemoveUnused<cr>", "Remove unused symbols" }
-  },
-}, { prefix = "<Leader>" })
+wk.add({
+  { "<Leader>t",  group = "Typescript commands" },
+  { "<Leader>tm", "<cmd>TypescriptAddMissingImports<cr>", desc = "Add missing imports" },
+  { "<Leader>tr", "<cmd>TypescriptRemoveUnused<cr>",      desc = "Remove unused symbols" },
+  { "<Leader>tt", "<cmd>TSC<cr>",                         desc = "Transpile TS" },
+})
