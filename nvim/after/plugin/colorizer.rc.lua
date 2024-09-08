d95e5a48d8e7cde8a16e12ae69b01af37fd56f5b
erodrigufer <erodrigufer@gmail.com>
2024-09-08 19:29:54 +0200

    nvim: Fix colorizer so that it is only used in webdev files.

--- nvim/after/plugin/colorizer.rc.lua
+++ nvim/after/plugin/colorizer.rc.lua
@@ -1,8 +1,8 @@
-local status, colorizer = pcall(require,'colorizer')
+local status, colorizer = pcall(require, 'colorizer')
 if (not status) then return end
 
-colorizer.setup {
-  '*'; -- Highlight all files, but customize some others.
-  '!vim'; -- Exclude vim from highlighting.
-  -- Exclusion Only makes sense if '*' is specified!
-}
+colorizer.setup({
+  css = { css = true, }, -- Enable parsing rgb(...) functions in css.
+  'html',
+  'javascript',
+})
