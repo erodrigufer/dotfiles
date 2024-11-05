d9ef034d36ec3b5abf3b727d76ef347fe1bc5c49
erodrigufer <erodrigufer@gmail.com>
2024-11-05 15:08:50 +0100

    nvim: Oil.nvim skips confirm and shows hidden files
    - Oil.nvim skips the confirmation message for "safe" actions.

--- nvim/after/plugin/oil.rc.lua
+++ nvim/after/plugin/oil.rc.lua
@@ -1,4 +1,9 @@
 local status, oil = pcall(require, "oil")
 if (not status) then return end
 
-oil.setup()
+oil.setup({
+  skip_confirm_for_simple_edits = true,
+  view_options = {
+    show_hidden = false,
+  },
+})
