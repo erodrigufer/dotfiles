3280476a736dd2133b20eae67a6db720e404210b
erodrigufer <erodrigufer@gmail.com>
2025-07-30 11:04:43 +0200

    feat(flash.nvim): Improve flash by enabling search and jump mode

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -137,7 +137,12 @@ require("lazy").setup({
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        search = { enabled = true },
+        char = { jump_labels = true },
+      },
+    },
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
       { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
